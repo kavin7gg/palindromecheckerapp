c0c61657d2da04bd9ded54794796a016aed7580d
kavipriyan <kavin29107@gmail.com>
2026-03-02 16:21:59 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -6,38 +6,26 @@ public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize input string
         String input = "civic";
-
-        // Create Queue (FIFO)
         Queue<Character> queue = new LinkedList<>();
-
-        // Create Stack (LIFO)
         Stack<Character> stack = new Stack<>();
 
-        // Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
-            queue.add(ch);     // Enqueue
-            stack.push(ch);    // Push
+            queue.add(ch);     
+            stack.push(ch);    
         }
-
-        // Assume palindrome initially
         boolean isPalindrome = true;
-
-        // Compare dequeue vs pop
         while (!queue.isEmpty()) {
 
-            char fromQueue = queue.remove();  // FIFO
-            char fromStack = stack.pop();     // LIFO
+            char fromQueue = queue.remove();  
+            char fromStack = stack.pop();     
 
             if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
