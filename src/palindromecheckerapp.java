ab1b4db2d58908a5b30bbfc609024a109469e35d
kavipriyan <kavin29107@gmail.com>
2026-03-02 16:24:13 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,44 +1,17 @@
-/**
- * ==========================================================
- * MAIN CLASS - UseCase10PalindromeCheckerApp
- * ==========================================================
- *
- * Use Case 10: Normalized Palindrome Validation
- *
- * Description:
- * This class validates a palindrome after preprocessing
- * the input string.
- *
- * Normalization includes:
- * - Removing spaces and symbols
- * - Converting to lowercase
- *
- * Example:
- * "A man a plan a canal Panama"
- *
- * @author Developer
- * @version 10.0
- */
+
 
 public class UseCase10PalindromeCheckerApp {
 
-    /**
-     * Application entry point for UC10.
-     * @param args Command-line arguments
-     */
+    
     public static void main(String[] args) {
 
-        // Define input string
         String input = "A man a plan a canal Panama";
 
-        // Normalize string:
-        // 1. Convert to lowercase
-        // 2. Remove non-alphanumeric characters
+    
         String normalized = input.toLowerCase().replaceAll("[^a-z0-9]", "");
 
         boolean isPalindrome = true;
 
-        // Compare characters from both ends
         for (int i = 0; i < normalized.length() / 2; i++) {
 
             if (normalized.charAt(i) !=
@@ -49,7 +22,6 @@ public class UseCase10PalindromeCheckerApp {
             }
         }
 
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
