4d28d14125bd6191d3393f28e9cde1099cd70700
kavipriyan <kavin29107@gmail.com>
2026-03-02 16:22:23 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -5,21 +5,16 @@ public class UseCase7PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Define the input string
         String input = "level";
 
-        // Create Deque
         Deque<Character> deque = new ArrayDeque<>();
 
-        // Insert characters into deque
         for (char c : input.toCharArray()) {
             deque.addLast(c);
         }
 
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Compare front and rear until deque is empty or one element left
         while (deque.size() > 1) {
 
             char front = deque.removeFirst();
@@ -30,8 +25,6 @@ public class UseCase7PalindromeCheckerApp {
                 break;
             }
         }
-
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
