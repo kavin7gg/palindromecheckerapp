eebb7aa4e864c7c6ecc6d5dc0ca7bb882f3365bb
kavipriyan <kavin29107@gmail.com>
2026-03-02 16:20:38 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -5,22 +5,13 @@ public class UseCase4PalindromeCheckerApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        // Take input
         System.out.print("Input text: ");
         String input = scanner.nextLine();
-
-        // Convert String to char array
         char[] characters = input.toCharArray();
-
-        // Two pointer technique
         int start = 0;
         int end = characters.length - 1;
-
         boolean isPalindrome = true;
-
         while (start < end) {
-
             if (characters[start] != characters[end]) {
                 isPalindrome = false;
                 break;
@@ -29,8 +20,6 @@ public class UseCase4PalindromeCheckerApp {
             start++;
             end--;
         }
-
-        // Display result
         System.out.println("Is it a Palindrome? : " + isPalindrome);
 
         scanner.close();
