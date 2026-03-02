76acc844ce9bed13da2c3da743573ab55edfef56
kavipriyan <kavin29107@gmail.com>
2026-03-02 16:21:13 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -3,22 +3,12 @@ import java.util.Stack;
 public class UseCase5PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        // Declare and initialize input string
         String input = "radar";
-
-        // Create Stack
         Stack<Character> stack = new Stack<>();
-
-        // Push characters into stack
         for (int i = 0; i < input.length(); i++) {
             stack.push(input.charAt(i));
         }
-
-        // Assume palindrome initially
         boolean isPalindrome = true;
-
-        // Pop characters and compare
         for (int i = 0; i < input.length(); i++) {
             char poppedChar = stack.pop();
 
@@ -28,7 +18,6 @@ public class UseCase5PalindromeCheckerApp {
             }
         }
 
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
